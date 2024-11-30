67bd3ba536a979f8fa51141940f58c0395db8b55
thisara samuditha <thellamburavithanagethisarasam@gmail.com>
2024-11-30 18:22:03 +0530

    added about section

--- portfolio.js
+++ portfolio.js
@@ -1,8 +1,7 @@
 var typed = new Typed(".text", {
-    strings: ["Frontend Developer", "Web Developer"],
+    strings: ["Engineer" ,"Frontend Developer", "Web Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 2000,
     loop: true
 });
-                
